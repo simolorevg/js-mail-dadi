2645ce25050f2e64d139abe511f5524265aee93b
simolorevg <simoneviaggi@libero.it>
2023-03-13 15:58:46 +0100

    script done

--- js-dadi/js/script.js
+++ js-dadi/js/script.js
@@ -1,1 +1,14 @@
-alert('Hello mondo');
+// dichiaro due variabili (cpu e giocatore) e sceglieranno un numero casuale tra 1 e 6 compresi
+const cpuPlayer = Math.floor( Math.random() * 6) + 1;
+const userPlayer = Math.floor( Math.random() * 6) + 1;
+let message;
+// se sono uguali stampo a schermo parità
+if( cpuPlayer === userPlayer){
+    message = "Parità";
+}else if(cpuPlayer > userPlayer){
+    message = "CPU VINCE!!!!";
+}else{
+    message = "UTENTE VINCE!!!!";
+}
+console.log(userPlayer, cpuPlayer);
+console.log(message);
