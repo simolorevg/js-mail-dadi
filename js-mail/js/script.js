15243276f9440caf06930a44e4dd44c234be6eae
simolorevg <simoneviaggi@libero.it>
2023-03-13 15:37:57 +0100

    main structrure of the script done

--- js-mail/js/script.js
+++ js-mail/js/script.js
@@ -1,8 +1,21 @@
 //UTENTE INSERISCE LA MAIL
 const submitButton = document.getElementById('submit-btn');
 const userMailInput = document.getElementById('mail');
+const mailArray = ["ciao@mail.it", "aldo@mail.it", "giovanni@mail.it", "giacomo@mail.it"];
+let message = "Mail non trovata";
 //CLICCANDO IL PULSANTE INVIA, IL PROGRAMMA ESEGUE LA VERIFICA
 submitButton.addEventListener("click", function(){
+   //PRENDO IL VALORE DI userMailInput
    const userMail = userMailInput.value;
-   console.log(userMail);
-})
+   //CERCO LA MAIL NELL'ARRAY
+   for(let i = 0; i < mailArray.length; i++){
+    //SE LA TROVO ASSEGNO A MESSAGE ACCESSO CONSENTITO
+    if( userMail === mailArray[i]){
+        message = "Accesso Consentito";
+    }
+    document.getElementById("answer").innerHTML = message;
+    userMailInput.value = "";
+   }
+});
+//STAMPO SU SCHERMO IL RISULTATO
+
